595435b9a14afaaaa3469b381cb802671c9f57f0
Graduacao Presencial <graduacao@acad.local>
2024-05-14 09:37:29 -0300

    Added dependencies in Netbeans

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -1,8 +1,12 @@
 package frontend;
 
+import console.Console;
+import mecanicas.Tabuleiro;
+import mecanicas.Carta;
+import cores.Cor;
 
 public class Main {
     public static void main(String[] args){
-        
+        Console.println("Ola Mundo " + "⿯");
     }
 }
