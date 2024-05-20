aafee47d0aa242960baf61c302806a04e4b70425
TiagoSM <tiagosantosmes@gmail.com>
2024-05-20 19:01:18 -0300

    Added pause menu and implemented the attack

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -5,10 +5,12 @@ import backend.Seas;
 import backend.Colors;
 import console.Console;
 import console.Tecla;
+import static console.Tecla.ESC;
 
 
 public class Main {
     public static void main(String[] args){
+        byte flag = 0;
         int rowCount = 0;
         int columnCount = 0;
         int rowAmount = 10;
@@ -16,21 +18,21 @@ public class Main {
         Colors string = new Colors("⚪");
         Boats boat = new Boats(string);
         Seas board = new Seas(rowAmount,columnAmount,boat);
-        //do{ //loop game
+        do{
+            Console.limpaTela();
             Console.println("MAIN MENU");
             Console.println("Click in ENTER to create a new game or click in S to load a saved game:");
-            if(Console.getTecla() == Tecla.ENTER){ //New Game
+            Tecla key = Console.getTecla();
+            if(key == Tecla.ENTER){ //New Game
                 do{
-               /*Enquanto está rodando deixar na  tela a opção de pausar com a tecla P, mostrando a tela de pause
-                onde se o usuário digitar B ele volta para o jogo, se ele digitar ele S ele salva o jogo em outro arquivo, 
-                se digitar M ele volta para o menu principal e se digitar ESC ele sai do jogo
-                */
+               //Enquanto está rodando deixar na  tela a tecla P para pausar
                Console.println("\tClick P to Pause");
                //Show the board, colocando os navios distintos em posições distintas
                Console.println(board);
                //Pedir uma jogada, se a jogada for inválida mostrar para o usuário e voltar o cursor para a posição 1
-               Console.print("Use the ARROWS for select the position and the ENTER for attack!\t");
-               Console.println("Attual position: (" + rowCount + ", " + columnCount + ").");
+               Console.print("ARROWS: Move\t");
+               Console.print("ENTER: Attack \t");
+               Console.println(" Position:(" + rowCount + "," + columnCount + ")");
                switch(Console.getTecla()){
                    case UP:
                        System.out.println("Clicou UP");
@@ -65,26 +67,60 @@ public class Main {
                        }
                        break;
                    case ENTER:
-                       board.Attack(rowCount, columnCount);
+                       int boatRowCount = 0;
+                       int boatColumnCount = 0;
+                       if(rowCount == boatRowCount && columnCount == boatColumnCount){
+                           board.SucesfullAttack(rowCount, columnCount);
+                       } else{
+                           board.MissedAttack(rowCount, columnCount);
+                       }
                        break;
-                       
+                   case P:
+                       //método que abre o menu de pausa
+                       /*opção de pausar com a tecla P mostrando a tela de pause
+                onde se o usuário digitar B ele volta para o jogo, se ele digitar ele S ele salva o jogo em outro arquivo, 
+                se digitar M ele volta para o menu principal e se digitar ESC ele sai do jogo
+                */
+                       Console.limpaTela();
+                       Console.println("Choose a option: ");
+                       Console.println("B: Continue");
+                       Console.println("S: Save Game");
+                       Console.println("M: Main Menu");
+                       Console.println("ESC: Exit Game");
+                       Tecla keyPause = Console.getTecla();
+                       switch(keyPause){
+                           case B:
+                               flag = 0;
+                               rowCount = 0;
+                               columnCount = 0;
+                               break;
+                           case M:
+                                flag = 1;
+                                break;
+                           case ESC:
+                                Console.saiDoPrograma();
+                                break;
+                       }
+                       break;
+                   
                    default:
-               
+                        Console.println("Enter a valid input!");
+                        rowCount = 0;
+                        columnCount = 0;
                }
-               //board.Attack();
                /*Verificar se o jogo terminou
                  Mostre uma mensagem de vitória com quem que ganhou
                  volte para o menu principal com a tecla M
                //salvar o jogo
                */
-               Console.limpaTela();
-                }while(Console.getTecla() != Tecla.M); //quando o usuário quiser voltar para o menu principal
-            } else{ //Saved Game
+                }while(flag != 1); //quando o usuário quiser voltar para o menu principal
+            } else if(key == Tecla.P){ //Saved Game
                 
+            } else{
+                Console.limpaTela();
+                Console.println("Enter a valid key!");
             }
             
-        //}while(Console.getTecla() != Tecla.ESC); //quando o usuário clicar ESC ele sai do jogo
-        Console.saiDoPrograma();
-        
+        }while(flag != 0); //faz o usuário voltar para o menu principal quando ele clicar em M
     }
 }
