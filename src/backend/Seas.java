dfbe787efc91579fe6db528fe31177fb2fbf8184
TiagoSM <tiagosantosmes@gmail.com>
2024-05-23 13:28:13 -0300

    Added the character from the boats

--- src/backend/Seas.java
+++ src/backend/Seas.java
@@ -6,8 +6,10 @@ import mecanicas.Tabuleiro;
 public class Seas extends Tabuleiro{
     Colors sunkenChar = new Colors("❌");
     Colors emptyChar = new Colors("⚫");
+    Colors boat = new Colors("");
     Boats sunkenBoat = new Boats(sunkenChar);
     Boats emptySea = new Boats(emptyChar);
+    Boats solidBoat = new Boats(boat);
     
     public Seas(int numLinhas, int numColunas, Carta fundo) {
         super(numLinhas, numColunas, fundo);
@@ -21,4 +23,7 @@ public class Seas extends Tabuleiro{
         setFundo(row, column, emptySea);
     }
     
+    public void Boat(int row, int column){
+        setFundo(row, column, solidBoat);
+    }
 }
