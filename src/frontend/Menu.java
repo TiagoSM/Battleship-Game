d6a42e097963e57bb7064959ae883af7725e60f2
TiagoSM <tiagosantosmes@gmail.com>
2024-06-01 12:18:32 -0300

    Completed the migration from structured programming to object-oriented programming

--- src/frontend/Menu.java
+++ src/frontend/Menu.java
@@ -1,40 +1,50 @@
 package frontend;
-//Testar o turn player no loop game
+
 import backend.Seas;
 import console.Console;
 import console.Tecla;
-import backend.Colors;
-import backend.Boat;
 
 public class Menu {
+    int i;
     boolean found = false;
     int boardPlayer1Selection;
     int boardPlayer2Selection;
     int column = 0;
     int row = 0;
-    byte count = 0;
+    byte countPlayer1 = 0;
+    byte countPlayer2 = 0;
     byte flag = 0;
     Tecla key;
+    Seas boardShip0;
+    Seas boardShip1;
     Seas boardPlayer1Default;
     Seas boardPlayer2Default;
     Seas boardPlayer1Action;
     Seas boardPlayer2Action;
 
-    //TOOLS
-    public void InvalidKey(Tecla key){
-        Console.println("You can't go to " + key + " now!");
-        Console.println("Click ENTER to continue");
+    public Menu(){
+        boardPlayer1Default = new Seas();
+        boardPlayer2Default = new Seas();
+        boardPlayer1Action = new Seas();
+        boardPlayer2Action = new Seas();
+        
+        boardShip0 = boardPlayer1Default.InitializeBoard("BOARD0");
+        boardShip1 = boardPlayer2Default.InitializeBoard("BOARD1");
     }
     
+    //TOOLS
     private void SelectBoard(int playerNumber, Seas boardPlayerDefault, int boardPlayerSelection){
         do{
                     do{
+                        Console.limpaTela();
                         Console.println("Player " + playerNumber + " are you ready to choose your ship board?");
                         Console.println("Click ENTER to continue:");
                     } while(Console.getTecla() != Tecla.ENTER);
                     
                     do{
-                        boardPlayerDefault.BoardOptions(column, new Seas("BOARD0"), new Seas("BOARD1"));
+                        Console.limpaTela();
+                        Console.println("Player " + playerNumber + " choice");
+                        boardPlayerDefault.BoardOptions(column, boardShip0, boardShip1);
                         key = Console.getTecla();
                         switch(key){
                             case LEFT:
@@ -55,62 +65,120 @@ public class Menu {
                 }while(key != Tecla.ENTER);
     }
     
-    private void TurnPlayer(Seas boardPlayerDefault, Seas boardPlayerAction){
+    public void PlayerAttack(int PlayerNumber, int boardPlayerSelection, Seas boardPlayerDefault, Seas boardPlayerAction, int rowCountPlayer, int columnCountPlayer){
+        if (boardPlayerSelection == 0) {
+            found = false;
+            for (i = 0; i < boardPlayerDefault.amountBoard0[0].length; i++) {
+                if (rowCountPlayer == boardPlayerDefault.amountBoard0[0][i] && columnCountPlayer == boardPlayerDefault.amountBoard0[1][i]) {
+                    found = true;
+                    break;
+                    }
+                }
+            if(PlayerNumber == 1){
+                if (found) {
+                    // Ataque bem-sucedido
+                    boardPlayerAction.SucesfullAttack(rowCountPlayer, columnCountPlayer);
+                    boardPlayer2Default.SucesfullAttack(rowCountPlayer, columnCountPlayer);
+                    if(PlayerNumber == 1){
+                       countPlayer1++;
+                       WinGame(countPlayer1);
+                    } else{
+                        countPlayer2++;
+                        WinGame(countPlayer2);
+                    }
+
+                } else {
+                    // Ataque falhou
+                    System.out.println("errou");
+                    boardPlayerAction.MissedAttack(rowCountPlayer, columnCountPlayer);
+                    boardPlayer2Default.MissedAttack(rowCountPlayer, columnCountPlayer);
+                }
+            } else if(PlayerNumber == 2){
+                if (found) {
+                    // Ataque bem-sucedido
+                    boardPlayerAction.SucesfullAttack(rowCountPlayer, columnCountPlayer);
+                    boardPlayer1Default.SucesfullAttack(rowCountPlayer, columnCountPlayer);
+                    if(PlayerNumber == 1){
+                       countPlayer1++;
+                       WinGame(countPlayer1);
+                    } else{
+                        countPlayer2++;
+                        WinGame(countPlayer2);
+                    }
+
+                } else {
+                    // Ataque falhou
+                    System.out.println("errou");
+                    boardPlayerAction.MissedAttack(rowCountPlayer, columnCountPlayer);
+                    boardPlayer1Default.MissedAttack(rowCountPlayer, columnCountPlayer);
+                }
+            }
+                
+        } else{
+            // Condição inicial falhou
+            System.out.println("tudo errado");
+        }
+    }
+    
+    private void TurnPlayer(int PlayerNumber, Seas boardPlayerDefault, Seas boardPlayerAction){
+        row = 0;
+        column = 0;
         do{
-                    do{
-                        Console.println("Player 1 are you ready to game?");
-                        Console.println("Click ENTER to continue:");
-                    }while(Console.getTecla() != Tecla.ENTER);
-                    
-                    BoardGame(boardPlayerDefault, boardPlayerAction, row, column);
-                    key = Console.getTecla();
-                    switch(key){
-                        case UP:
-                            row = boardPlayerAction.RowMovementUp(row, key);
-                            break;
-                        case DOWN:
-                            row = boardPlayerAction.RowMovementDown(row, key);
-                            break;
-                        case LEFT:
-                            column = boardPlayerDefault.ColumnMovementLeft(column, key);
-                            break;
-                        case RIGHT:
-                            column = boardPlayerDefault.ColumnMovementRight(column, key, 10);
-                            break;
-                        case ENTER:
-                            flag = 1;
-                            boardPlayerAction.PlayerAttack(boardPlayer1Selection, boardPlayerDefault, boardPlayerAction, column, row);
+            Console.limpaTela();
+            Console.println("Player " + PlayerNumber + " are you ready to game?");
+            Console.println("Click ENTER to continue:");
+        }while(Console.getTecla() != Tecla.ENTER);
+        
+        do{       
+            BoardGame(PlayerNumber, boardPlayerDefault, boardPlayerAction, row, column);
+        try{
+            key = Console.getTecla();
+            switch(key){
+                case UP:
+                    row = boardPlayerAction.RowMovementUp(row, key);
+                    break;
+                case DOWN:
+                    row = boardPlayerAction.RowMovementDown(row, key);
+                    break;
+                case LEFT:
+                    column = boardPlayerAction.ColumnMovementLeft(column, key);
+                    break;
+                case RIGHT:
+                    column = boardPlayerAction.ColumnMovementRight(column, key, 9);
+                    break;
+                case ENTER:
+                    PlayerAttack(PlayerNumber, boardPlayer1Selection, boardPlayerDefault, boardPlayerAction, row, column);
+                    break;
+                case P:
+                    PauseMenu();
+                    Tecla keyPause = Console.getTecla();
+                    switch(keyPause){
+                        case B:
+                            flag = 0;
+                            row = 0;
+                            column = 0;
                             break;
                         case P:
-                            PauseMenu();
-                            Tecla keyPause = Console.getTecla();
-                            switch(keyPause){
-                                case B:
-                                    flag = 0;
-                                    row = 0;
-                                    column = 0;
-                                    break;
-                                case P:
-                                    break;
-                                case M:
-                                     flag = 1;
-                                     break;
-                                case ESC:
-                                     Console.saiDoPrograma();
-                                     break;
-                            }
                             break;
-
-                        default:
-                             Console.println("Enter a valid input!");
-                             row = 0;
-                             column = 0;
+                        case M:
+                            flag = 1;
+                            break;
+                        case ESC:
+                            Console.saiDoPrograma();
+                            break;
                     }
-                    //Tela de que ganhou o jogo
-                    WinGame();
+                    break;
+
+                default:
+                    row = 0;
+                    column = 0;
+                    }
+            } catch(RuntimeException e){
+                Console.println("Enter a valid input!");
+            }
                     
                     Console.limpaTela();
-                }while(flag != 1);
+                }while(key != Tecla.ENTER);
     }
     
     
@@ -138,16 +206,18 @@ public class Menu {
         Console.println("ESC: Exit Game");
     }
     
-    private void BoardGame(Seas boardPlayerDefault, Seas boardPlayerAction, int rowCountPlayer, int columnCountPlayer){
-         Console.println("\tClick P to Pause");
-         Console.print(boardPlayerDefault);
-         Console.println(boardPlayerAction);
-         Console.print("ARROWS: Move\t");
-         Console.print("ENTER: Attack \t");
-         Console.println(" Position:(" + rowCountPlayer + "," + columnCountPlayer + ")");
+    private void BoardGame(int PlayerNumber, Seas boardPlayerDefault, Seas boardPlayerAction, int rowCountPlayer, int columnCountPlayer){
+        Console.limpaTela();
+        Console.println("Player " + PlayerNumber + " turn");
+        Console.println("\tClick P to Pause");
+        Console.print(boardPlayerDefault);
+        Console.println(boardPlayerAction);
+        Console.print("ARROWS: Move\t");
+        Console.print("ENTER: Attack \t");
+        Console.println(" Position:(" + rowCountPlayer + "," + columnCountPlayer + ")");
     }
     
-    private void WinGame(){
+    private void WinGame(int count){
         if(count == 30){
             WinMenu();
             Tecla keyPause = Console.getTecla();
@@ -162,27 +232,18 @@ public class Menu {
     }
     
     //PHASES
-    public void ChooseBoard(){
-        boardPlayer1Default = new Seas("BOARD0");
-        boardPlayer2Default = new Seas("BOARD1");
-        
+    public void ChooseBoard(){        
         SelectBoard(1, boardPlayer1Default, boardPlayer1Selection);
         SelectBoard(2, boardPlayer2Default, boardPlayer2Selection);
     }
     
     public void LoopGame(){
-        
         do{
-            
-                
-                //loop game
                 //player 1 attack
-                TurnPlayer(boardPlayer1Default, boardPlayer2Action);
-                //player 1 win?
-                WinGame();
+                TurnPlayer(1, boardPlayer1Default, boardPlayer2Action);
 
                 //player 2 attack
-                TurnPlayer(boardPlayer2Default, boardPlayer1Action);
+                TurnPlayer(2, boardPlayer2Default, boardPlayer1Action);
         }while(flag != 9);
         
     }
