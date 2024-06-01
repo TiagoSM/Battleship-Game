f494069adef58dd6166fccad7f286e88290fa96f
TiagoSM <tiagosantosmes@gmail.com>
2024-06-01 12:20:57 -0300

    Removed global variables and libraries that were not being used

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -1,7 +1,6 @@
 package frontend;
-//Criar métodos dentro do Menu para chamar na Main
+
 import console.Console;
-import console.Tecla;
 
 public class Main {
     public static void main(String[] args){
@@ -10,14 +9,17 @@ public class Main {
         while(true){
             display.MainMenu();
             display.key = Console.getTecla();
-            if(display.key == Tecla.ENTER){ //New Game
-                display.ChooseBoard();
-                display.LoopGame();
-                
-            } else if(display.key == Tecla.S){
-                
-            } else if(display.key == Tecla.ESC){
-                
+            switch(display.key){
+                case ENTER:
+                    display.ChooseBoard();
+                    display.LoopGame();
+                    break;
+                case S:
+                    Console.limpaTela();
+                    //Add Save Game
+                    break;
+                case ESC:
+                    Console.saiDoPrograma();
             }
         }
     }
